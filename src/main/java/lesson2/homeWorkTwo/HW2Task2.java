6bb5accf42321c22f30a3ec23ffdf073cd81e890
Anastasiya Mironenko <nastya_911@mail.ru>
2023-03-29 23:09:45 +0300

    задача: записать текст в файл

---
+++ src/main/java/lesson2/homeWorkTwo/HW2Task2.java
@@ -0,0 +1,22 @@
+package lesson2.homeWorkTwo;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+/*ЗАДАЧА 2. Напишите метод, который составит строку, состоящую из 100 повторений слова TEST и метод,
+ который запишет эту строку в простой текстовый файл, обработайте исключения.*/
+public class HW2Task2 {
+    public static void main(String[] args) {
+        test();
+    }
+
+    private static void test() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("TEST ".repeat(100));
+
+        try (PrintWriter pw = new PrintWriter("src/main/resources/output.txt")) {
+            pw.print(sb);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
