9172214bd4acc541c5a1bc2e03e54b9efe49e6ff
Anastasiya Mironenko <nastya_911@mail.ru>
2023-04-07 14:40:27 +0300

    Map: работа в группах

--- src/main/java/lesson5/MainClassEx1.java
+++ src/main/java/lesson5/MainClassEx1.java
@@ -1,15 +1,26 @@
 package lesson5;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class MainClassEx1 {
     public static void main(String[] args) {
-        //ex1();
-        System.out.println("MMCMXCIX -> " + ex3("MMCMXCIX"));
+        String number = "MMCMXCIX";
+        System.out.println(fromRomanToArabic(number));
+        //System.out.println("MMCMXCIX -> " + ex3("MMCMXCIX"));
     }
 
-    // РЕШЕНИЕ В ГРУППАХ НА СЕМИНАРЕ (без шапки):
-    /*public static void ex1() {
+    // РЕШЕНИЕ В ГРУППАХ НА СЕМИНАРЕ:
+    private static int fromRomanToArabic(String num) {
+        Map<String, Integer> numbers = new HashMap<>();
+        numbers.put("I", 1);
+        numbers.put("V", 5);
+        numbers.put("X", 10);
+        numbers.put("L", 50);
+        numbers.put("C", 100);
+        numbers.put("D", 500);
+        numbers.put("M", 1000);
+
         int sumNum = 0;
         int next = -1;
         for (int i = 0; i < num.length() - 1; i++) {
@@ -24,11 +35,10 @@ public class MainClassEx1 {
         sumNum += next;
         return sumNum;
     }
+}
 
-    }
-}*/
-    // МЕТОД ПРЕПОДАВАТЕЛЯ:
-    private static Integer ex3(String romanNum) {
+// МЕТОД ПРЕПОДАВАТЕЛЯ:
+   /* private static Integer ex3(String romanNum) {
 
         Map<Character, Integer> romanToDecimalMap = Map.of(
                 'I', 1,
@@ -54,4 +64,4 @@ public class MainClassEx1 {
         }
         return decimalNum;
     }
-}
+}*/
